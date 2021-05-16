dc71a6e4b70ea12c645852980f882e056ff9c339
Qohap <st.william24@gmail.com>
2021-05-16 18:14:12 +0200

    basic classes

--- src/main.cpp
+++ src/main.cpp
@@ -18,26 +18,26 @@ int main()
 
 
 
-//	std::cout << midifile << std::endl;
+ 	std::cout << midifile << std::endl;
 
-	int tracks = midifile.getTrackCount();
-	std::cout << "TPQ: " << midifile.getTicksPerQuarterNote() << std::endl;
-	if (tracks > 1) std::cout << "TRACKS: " << tracks << std::endl;
-	for (int track=0; track<tracks; track++) {
-		if (tracks > 1) std::cout << "\nTrack " << track << std::endl;
-		std::cout << "Tick\tSeconds\tDur\tMessage" << std::endl;
-		for (int event=0; event<midifile[track].size(); event++) {
-			std::cout << std::dec << midifile[track][event].tick;
-			std::cout << '\t' << std::dec << midifile[track][event].seconds;
-			std::cout << '\t';
-			if (midifile[track][event].isNoteOn())
-				std::cout << midifile[track][event].getDurationInSeconds();
-			std::cout << '\t' << std::hex;
-			for (int i=0; i<midifile[track][event].size(); i++)
-				std::cout << (int)midifile[track][event][i] << ' ';
-			std::cout << std::endl;
-		}
-	}
+//	int tracks = midifile.getTrackCount();
+//	std::cout << "TPQ: " << midifile.getTicksPerQuarterNote() << std::endl;
+//	if (tracks > 1) std::cout << "TRACKS: " << tracks << std::endl;
+//	for (int track=0; track<tracks; track++) {
+//		if (tracks > 1) std::cout << "\nTrack " << track << std::endl;
+//		std::cout << "Tick\tSeconds\tDur\tMessage" << std::endl;
+//		for (int event=0; event<midifile[track].size(); event++) {
+//			std::cout << std::dec << midifile[track][event].tick;
+//			std::cout << '\t' << std::dec << midifile[track][event].seconds;
+//			std::cout << '\t';
+//			if (midifile[track][event].isNoteOn())
+//				std::cout << midifile[track][event].getDurationInSeconds();
+//			std::cout << '\t' << std::hex;
+//			for (int i=0; i<midifile[track][event].size(); i++)
+//				std::cout << (int)midifile[track][event][i] << ' ';
+//			std::cout << std::endl;
+//		}
+//	}
 
 //	srand( (unsigned)time(nullptr) );
 //
