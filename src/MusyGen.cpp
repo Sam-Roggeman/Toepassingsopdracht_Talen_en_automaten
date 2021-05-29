f301319de09f679f9bd7a078a6b8a21dccedd36a
Sam-Roggeman <sam.roggeman@student.uantwerpen.be>
2021-05-29 14:56:52 +0200

    pause fixx 2.0

--- src/MusyGen.cpp
+++ src/MusyGen.cpp
@@ -692,8 +692,15 @@ int MusyGen::findMinDelay(std::vector<Note>& note_group)
 
 void MusyGen::pauseMessage(RtMidiOut *pOut) {
     std::vector<unsigned char> message;
+    message.emplace_back(0b10110000);
+    message.emplace_back(0xFF & 124);
+    message.emplace_back(0xFF & 0);
+    pOut->sendMessage(&message);
+    message.resize(0);
     message.emplace_back(0b11111100);
     pOut->sendMessage(&message);
+
+
 }
 void MusyGen::startMessage(RtMidiOut *pOut) {
     std::vector<unsigned char> message;
